e45ee528bc49acea289357f2b7d5d3adccc47b27
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-09-04 22:03:41 -0300

    :recycle: refactor: Refatora página de login
    Altera página de login para adicionar fluxo de registro

--- front-end/src/pages/Login/index.jsx
+++ front-end/src/pages/Login/index.jsx
@@ -1,21 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { getItemLocalStorage, setItemLocalStorage } from '../../helpers/localStorage';
-import {
-  validateEmailAndPassword } from '../../helpers/validate/validateEmailAndPassword';
 import { fetchPost } from '../../helpers/api/requests';
-import deliveryBackground from '../../images/TakeAway-pana.svg';
+import validateDataUser from '../../helpers/validate/validateDataUser';
+import registerIcon from '../../images/icons/register.svg';
+import loginIcon from '../../images/icons/login.svg';
+import cloudsIlustration from '../../images/ilustrations/clouds.svg';
+import beerIcon from '../../images/icons/beer-icon.png';
 import './style.css';
 
 function LoginPage() {
-  const [login, setLogin] = useState({
+  const [dataUser, setDataUser] = useState({
+    name: '',
     email: '',
     password: '',
   });
   const [buttonDisabled, setButtonDisabled] = useState(true);
   const [messageError, setMessageError] = useState('');
+  const [isRegister, setIsRegister] = useState(false);
 
   const history = useHistory();
+  const pathname = history.location.pathname.replace('/', '');
+
+  const validateUser = () => {
+    if (isRegister) {
+      setButtonDisabled(!validateDataUser(dataUser, true));
+    } else {
+      const dataLogin = { email: dataUser.email, password: dataUser.password };
+      setButtonDisabled(!validateDataUser(dataLogin));
+    }
+  };
 
   const isUserLogged = () => {
     const findUser = getItemLocalStorage('user');
@@ -41,18 +55,20 @@ function LoginPage() {
 
   // componentDidUpdate
   useEffect(() => {
-    setButtonDisabled(validateEmailAndPassword(login.email, login.password));
-  }, [login]);
+    validateUser();
+  }, [dataUser]);
 
   const handleChange = ({ target: { name, value } }) => {
-    setLogin({
-      ...login,
+    setDataUser({
+      ...dataUser,
       [name]: value,
     });
   };
 
   const postLogin = async () => {
-    const response = await fetchPost(login, 'login');
+    const dataLogin = { email: dataUser.email, password: dataUser.password };
+    const response = isRegister ? await fetchPost(dataUser, 'register') : (
+      await fetchPost(dataLogin, 'login'));
 
     if (response.message) {
       setMessageError(response.message);
@@ -73,17 +89,48 @@ function LoginPage() {
 
   return (
     <div className="login">
-      <div className="login-form">
+      <div className="container-form">
+        <div className="presentation">
+          <h1>
+            <img src={ beerIcon } alt="Icone de uma garrafa de cerveja" />
+            Devlivery
+          </h1>
+          <p>Seja Bem Vindo! Faça seu login na plataforma</p>
+        </div>
+        <div className="select-form">
+          <button type="button" onClick={ () => setIsRegister(false) }>
+            <img src={ loginIcon } alt="ícone de login" />
+            <span>Entrar</span>
+          </button>
+          <button type="button" onClick={ () => setIsRegister(true) }>
+            <img src={ registerIcon } alt="ícone de registro" />
+            <span>Registrar</span>
+          </button>
+        </div>
         <form>
+          { isRegister && (
+            <label htmlFor="input-name">
+              Nome
+              <input
+                data-testid="common_register__input-name"
+                type="text"
+                name="name"
+                placeholder="Seu nome"
+                value={ dataUser.name }
+                onChange={ handleChange }
+              />
+            </label>
+          )}
           <label htmlFor="email">
             Email
             <input
               id="email"
               type="text"
               name="email"
+              spellCheck="false"
               placeholder=" email@email.com"
-              value={ login.email }
-              data-testid="common_login__input-email"
+              value={ dataUser.email }
+              data-testid={ `common_${pathname}__input-email` }
               onChange={ handleChange }
             />
           </label>
@@ -93,42 +140,33 @@ function LoginPage() {
               id="password"
               type="password"
               placeholder=" * * * * * *"
-              data-testid="common_login__input-password"
+              data-testid={ `common_${pathname}__input-password` }
               name="password"
-              value={ login.password }
+              value={ dataUser.password }
               onChange={ handleChange }
             />
           </label>
           <button
+            className="btn-primary"
             type="button"
-            data-testid="common_login__button-login"
+            data-testid={ `common_${pathname}__button-${pathname}` }
             disabled={ buttonDisabled }
             onClick={ postLogin }
+            style={ { cursor: buttonDisabled ? 'not-allowed' : 'pointer' } }
           >
-            login
+            { isRegister ? 'registrar' : 'entrar' }
           </button>
-          <div>
-            <Link
-              to="/register"
-              type="button"
-              data-testid="common_login__button-register"
-            >
-              Ainda não tenho conta
-            </Link>
-          </div>
           <span
-            data-testid="common_login__element-invalid-email"
+            data-testid={ pathname === 'login' ? 'common_login__element-invalid-email' : (
+              'common_register__element-invalid-register') }
             style={ messageError.length > 0 ? { opacity: 1 } : { opacity: 0 } }
           >
             {messageError}
           </span>
         </form>
       </div>
-      <div className="login-background">
-        <h1>Delivery App</h1>
-        <figure>
-          <img src={ deliveryBackground } alt="" />
-        </figure>
+      <div className="login-branding">
+        <img src={ cloudsIlustration } alt="ilustração de nuvens" />
       </div>
     </div>
   );
