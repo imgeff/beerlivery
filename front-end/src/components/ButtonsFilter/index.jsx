8620f1c8bd439df4a0a7e6bcee503aa468590864
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-04 23:30:04 -0300

    :sparkles: feat: Implementa componente ButtonsFilter
    responsável pelo filtro dos produtos

---
+++ front-end/src/components/ButtonsFilter/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './style.css';
+
+function ButtonsFilter({ filter: { brandings, filterByBrand } }) {
+  return (
+    <div className="buttons-filter">
+      <button
+        id="filter-branding-0"
+        className="button-filter active-filter-quaternary"
+        type="button"
+        onClick={ () => {
+          filterByBrand('', '#filter-branding-0');
+        } }
+      >
+        Sem filtro
+      </button>
+      { brandings.map(({ id, name }) => (
+        <button
+          key={ id }
+          id={ `filter-branding-${id}` }
+          className="button-filter"
+          type="button"
+          onClick={ () => filterByBrand(name, `#filter-branding-${id}`) }
+        >
+          { name }
+        </button>
+      ))}
+    </div>
+  );
+}
+
+ButtonsFilter.propTypes = {
+  filter: PropTypes.shape({
+    filterByBrand: PropTypes.func,
+    brandings: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+    })),
+  }).isRequired,
+};
+
+export default ButtonsFilter;
