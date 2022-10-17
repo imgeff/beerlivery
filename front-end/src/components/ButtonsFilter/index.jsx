7a3779fb62b3ad5cfce32cbddd86b57723d7e0b5
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-10-16 22:48:25 -0300

    :iphone: feat: Adiciona reajustes de layout para diferentes telas

--- front-end/src/components/ButtonsFilter/index.jsx
+++ front-end/src/components/ButtonsFilter/index.jsx
@@ -3,29 +3,41 @@ import PropTypes from 'prop-types';
 import './style.css';
 
 function ButtonsFilter({ filter: { brandings, filterByBrand } }) {
+  const mouseScrollFilters = (event) => {
+    if (event.deltaY > 0) {
+      event.target.scrollBy(160, 0)
+    } else {
+      event.target.scrollBy(-160, 0)
+    }
+  }
+
   return (
-    <div className="buttons-filter">
-      <button
-        id="filter-branding-0"
-        className="button-filter active-filter-quaternary"
-        type="button"
-        onClick={ () => {
-          filterByBrand('', '#filter-branding-0');
-        } }
-      >
-        Sem filtro
-      </button>
-      { brandings.map(({ id, name }) => (
-        <button
-          key={ id }
-          id={ `filter-branding-${id}` }
-          className="button-filter"
-          type="button"
-          onClick={ () => filterByBrand(name, `#filter-branding-${id}`) }
-        >
-          { name }
-        </button>
-      ))}
+    <div className="buttons-wrapper" onWheel={ mouseScrollFilters }>
+      <div>
+        <div className="buttons-filter">
+          <button
+            id="filter-branding-0"
+            className="button-filter active-filter-quaternary"
+            type="button"
+            onClick={ () => {
+              filterByBrand('', '#filter-branding-0');
+            } }
+          >
+            Sem filtro
+          </button>
+          { brandings.map(({ id, name }) => (
+            <button
+              key={ id }
+              id={ `filter-branding-${id}` }
+              className="button-filter"
+              type="button"
+              onClick={ () => filterByBrand(name, `#filter-branding-${id}`) }
+            >
+              { name }
+            </button>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
